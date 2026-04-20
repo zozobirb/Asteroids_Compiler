1f3d757c21d41b2caceb1084c1e1baa7772d8953
Zoe Samudio <zozobirdx2@gmail.com>
2026-04-20 13:21:46 -0500

    Added AsteroidServices interface and implementation class that implements it, no real logic, will continue to work on that. Also modified the Engine class, use an example of a query to the api. Will continue to work on both!

--- Engine.java
+++ Engine.java
@@ -1,7 +1,49 @@
-public abstract class Engine {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class Engine {
 
 //API REQUESTS HERE
 
 //WE WNAT RESULTS ONLY NO EXTRA STUFF VISITORS DEAL WITH THAT!!!
+//API CALLS there are three
+//We have, Feed, Browse, Lookup
 
+//get distance can be refrence either name or id!!
+public String getDistance(int id){
+try {
+           
+  //http request here if we have an id use NEOWS: Look-up
+//parameter asteroid_id, 
+//Query: https://api.nasa.gov/neo/rest/v1/neo/ID?api_key=DEMO_KEY 
+//
+//for now hard coding the id
+id = 3542519;
+String url = "https://api.nasa.gov/neo/rest/v1/neo/"+id+"?api_key=DEMO_KEY";
+
+ HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            
+            System.out.println("Raw JSON response:");
+            System.out.println(response.body());
+
+            return response.body();
+
+
+} } catch (Exception e) {
+            e.printStackTrace();
+            return null; //throw exceptions
+        }
 }
+
+
